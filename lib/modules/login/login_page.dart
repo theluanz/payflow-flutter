005dce28619a7cbe5dc88c1014b6a7a4cf1d3561
theluanz <luanzuffo.lz@gmail.com>
2022-07-13 15:39:48 -0300

    Auth with Google

--- lib/modules/login/login_page.dart
+++ lib/modules/login/login_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:payflow/modules/login/login_controller.dart';
 import 'package:payflow/shared/themes/app_colors.dart';
 import 'package:payflow/shared/themes/app_images.dart';
 import 'package:payflow/shared/themes/app_text_styles.dart';
@@ -12,6 +13,8 @@ class LoginPage extends StatefulWidget {
 }
 
 class _LoginPageState extends State<LoginPage> {
+  final loginController = LoginController();
+
   @override
   Widget build(BuildContext context) {
     final size = MediaQuery.of(context).size;
@@ -52,13 +55,11 @@ class _LoginPageState extends State<LoginPage> {
                         textAlign: TextAlign.center),
                   ),
                   Padding(
-                    padding: const EdgeInsets.only(top: 40, left: 40, right: 40),
-                    child: SocialLoginButton(
-                      onTap: (){
-                        print("clicou");
-
-                      }
-                    ),
+                    padding:
+                        const EdgeInsets.only(top: 40, left: 40, right: 40),
+                    child: SocialLoginButton(onTap: () {
+                      loginController.googleSignIn(context);
+                    }),
                   )
                 ],
               ),
@@ -66,5 +67,4 @@ class _LoginPageState extends State<LoginPage> {
           ]),
         ));
   }
-  
 }
